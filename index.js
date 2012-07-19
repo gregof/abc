0e9349ba8ed1dbd5a33a53eef7590ed8765c610c
Golubev Fedor <gregof@yandex.ru>
2012-07-19 11:55:32 +0400

    add functions

---
+++ index.js
@@ -0,0 +1,5 @@
+exports.async = require("./lib/async.js");
+exports.dir = require("./lib/dir.js");
+exports.executer = require("./lib/executer.js");
+exports.file = require("./lib/file.js");
+exports.fileFinder = require("./lib/fileFinder.js");
