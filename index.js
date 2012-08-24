fd62daaedab8c63c895732cea287e9e8c5eb5fc0
Golubev Fedor <gregof@yandex.ru>
2012-08-24 19:04:35 +0400

    fileFinder -> find; module.exports -> exports

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ exports.async = require("./lib/async.js");
 exports.dir = require("./lib/dir.js");
 exports.executer = require("./lib/executer.js");
 exports.file = require("./lib/file.js");
-exports.fileFinder = require("./lib/fileFinder.js");
+exports.find = require("./lib/fileFinder.js");
