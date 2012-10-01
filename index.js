f4923113750634f0c08e1b5cc8a83ba33094a801
Fedor Golubev <fgolubev@yandex-team.ru>
2012-10-01 13:59:52 +0400

    provide 'extend' to common 'abc' namespace

--- index.js
+++ index.js
@@ -3,3 +3,4 @@ exports.dir = require("./lib/dir.js");
 exports.executer = require("./lib/executer.js");
 exports.file = require("./lib/file.js");
 exports.find = require("./lib/find.js");
+exports.extend = require("./lib/extend.js");
