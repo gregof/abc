0995faa6b15d88c6df2e7926165524f5cbafc915
Fedor Golubev <fgolubev@yandex-team.ru>
2013-08-13 16:50:51 +0400

    fixes for KidsKilla's pull request (https://github.com/gregof/abc/pull/3)

--- test/async/for-each-object-2args.case.js
+++ test/async/for-each-object-2args.case.js
@@ -1,16 +1,20 @@
 //in
 var async = require(tc.fixPath('../../lib/async.js'));
 var arr = {
-  one: function () {return 1}, 
-  two: function () {return 2}, 
-  three: function () {return 3}
+    one: function (callback) {callback(1);},
+    two: function (callback) {
+        setTimeout(function () {
+            callback(2);
+        }, 200);
+    },
+    three: function (callback) {callback(3);}
 };
 async.forEach(
     arr,
     function (autoResults) {
-        tc.out([results.one, results.two, results.three]);
+        tc.out([autoResults.one, autoResults.two, autoResults.three]);
         tc.finish();
     }
-)
+);
 //out
 1,2,3
--- test/async/for-each-object-3args.case.js
+++ test/async/for-each-object-3args.case.js
@@ -1,15 +1,21 @@
 //in
 var async = require(tc.fixPath('../../lib/async.js'));
-var arr = {one:1, two:2, three:3};
+var arr = {one: 1, two: 2, three: 3};
 async.forEach(
     arr,
     function (value, callback) {
-        callback(value * 2);
+        if (value === 1) {
+            setTimeout(function () {
+                callback(value * 2);
+            }, 200)
+        } else {
+            callback(value * 2);
+        }
     },
     function (autoResults) {
-        tc.out([results.one, results.two, results.three]);
+        tc.out([autoResults.one, autoResults.two, autoResults.three]);
         tc.finish();
     }
-)
+);
 //out
 2,4,6
